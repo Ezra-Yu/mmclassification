7a9c5592f77ddad417d12efeac511c952348c5a5
Ezra-Yu <1105212286@qq.com>
2022-01-28 18:52:00 +0800

    merge dev

---
+++ configs/resnet/resnet50_8xb128-vision.py
@@ -0,0 +1,16 @@
+_base_ = 'resnet50_8xb32-coslr_in1k.py'
+
+# Precise BN hook will update the bn stats, so this hook should be executed
+# before CheckpointHook, which has priority of 'NORMAL'. So set the
+# priority of PreciseBNHook to 'ABOVE_NORMAL' here.
+custom_hooks = [
+    dict(
+        type='PreciseBNHook',
+        num_samples=8192,
+        interval=1,
+        priority='ABOVE_NORMAL')
+]
+
+adjust = 8 * 128 * 32 / 600
+alpha = 1.0 - args.model_ema_decay
+alpha = min(1.0, alpha * adjust)

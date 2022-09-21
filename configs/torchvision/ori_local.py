e5aaecb6239ac4b690a62ba720988ddfcfb2d559
Ezra-Yu <18586273+Ezra-Yu@users.noreply.github.com>
2022-09-21 21:01:40 +0800

    add vision

---
+++ configs/torchvision/ori_local.py
@@ -0,0 +1,108 @@
+_base_ = ['../_base_/models/resnet50.py', '../_base_/default_runtime.py']
+
+# model settings
+model = dict(
+    head=dict(
+        loss=dict(
+            type='LabelSmoothLoss',
+            label_smooth_val=0.1,
+            mode='original',
+        )),
+    train_cfg=dict(augments=[
+        dict(type='Mixup', alpha=0.2, num_classes=1000),
+        dict(type='CutMix', alpha=1.0, num_classes=1000)
+    ]),
+)
+
+# dataset settings
+dataset_type = 'VisionImageNet'
+preprocess_cfg = dict(
+    mean=[0., 0., 0.],
+    std=[1., 1., 1.],
+    to_rgb=False,
+)
+
+train_dataloader = dict(
+    batch_size=12,
+    num_workers=8,
+    dataset=dict(
+        type=dataset_type,
+        ann_file='./data/imagenet/meta/train.txt',
+        data_prefix='./data/imagenet/train'),
+    sampler=dict(type='RepeatAugSampler', shuffle=True, num_repeats=4),
+    persistent_workers=True,
+)
+
+val_dataloader = dict(
+    batch_size=64,
+    num_workers=8,
+    dataset=dict(
+        type=dataset_type,
+        data_prefix='./data/imagenet/val',
+        ann_file='./data/imagenet/meta/val.txt'),
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    persistent_workers=True,
+)
+val_evaluator = dict(type='Accuracy', topk=(1, 5))
+
+# If you want standard test, please manually configure the test dataset
+test_dataloader = val_dataloader
+test_evaluator = val_evaluator
+
+# optimizer
+optim_wrapper = dict(
+    optimizer=dict(type='SGD', lr=0.5, momentum=0.9, weight_decay=2e-5),
+    paramwise_cfg=dict(norm_decay_mult=0.),
+)
+
+param_scheduler = [
+    # warm up learning rate scheduler
+    dict(
+        type='LinearLR',
+        start_factor=0.001,
+        by_epoch=True,
+        begin=0,
+        end=6),
+    # main learning rate scheduler
+    dict(
+        type='CosineAnnealingLR',
+        eta_min=1.0e-6,
+        by_epoch=True,
+        begin=6)
+]
+
+# train, val, test setting
+train_cfg = dict(by_epoch=True, max_epochs=600, val_interval=1)
+val_cfg = dict()
+test_cfg = dict()
+
+# custom_hooks = [
+#     dict(
+#         type='EMAHook',
+#         momentum=0.0010923,
+#         begin_epoch=5,
+#         interval=32,
+#         update_buffers=True,
+#         priority='ABOVE_NORMAL')
+# ]
+
+# decay(torchvision) = 1 - momentum
+# custom_hooks = [
+
+#     # Decay adjustment that aims to keep the decay independent from other hyper-parameters originally proposed at:
+#     # https://github.com/facebookresearch/pycls/blob/f8cd9627/pycls/core/net.py#L123
+#     #
+#     # total_ema_updates = (Dataset_size / n_GPUs) * epochs / (batch_size_per_gpu * EMA_steps) = [(1287056 / 8) * 600] / (128 * 32) = 23567
+#     # We consider constant = Dataset_size for a given dataset/setup and ommit it. Thus:
+#     # adjust = 1 / total_ema_updates ~= n_GPUs * batch_size_per_gpu * EMA_steps / epochs = 8 * 128 * 32 * 23567 / 600 = 1287072
+#     # adjust = args.world_size * args.batch_size * args.model_ema_steps / args.epochs  = 8 * 128 * 32 / 600 = 54.61333333333334
+#     # alpha = 1.0 - args.model_ema_decay     # 2-05   0.00002 
+#     # alpha = min(1.0, alpha * adjust)  # min(1, 0.00002 * 54.61333333333334 = 2.56e-5) = 0.0010923
+#     dict(
+#         type='LazyEMAHook',
+#         momentum=0.0010923,
+#         lazy_interal=5,
+#         interval=32,
+#         update_buffers=True,
+#         priority='ABOVE_NORMAL')
+# ]
